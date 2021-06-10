624accdcaf04e8ab3c160636625c593703fb9ea4
AtishGhosh <atishghosh30@gmail.com>
2021-06-10 23:42:32 +0530

    upgrade pkg 21

--- js/projects.js
+++ js/projects.js
@@ -1,6 +1,50 @@
+function collapseSwitch1() {
+    console.log(document.getElementById("btn-1").innerText);
+    if (document.getElementById("btn-1").innerText === 'Learn More') {
+        document.getElementById("btn-1").innerText="Collapse";
+    } else {
+        document.getElementById("btn-1").innerText="Learn More";
+    }
+}
+
+function collapseSwitch2() {
+    console.log(document.getElementById("btn-2").innerText);
+    if (document.getElementById("btn-2").innerText === 'Learn More') {
+        document.getElementById("btn-2").innerText="Collapse";
+    } else {
+        document.getElementById("btn-2").innerText="Learn More";
+    }
+}
+
+function collapseSwitch3() {
+    console.log(document.getElementById("btn-3").innerText);
+    if (document.getElementById("btn-3").innerText === 'Learn More') {
+        document.getElementById("btn-3").innerText="Collapse";
+    } else {
+        document.getElementById("btn-3").innerText="Learn More";
+    }
+}
+
+function collapseSwitch4() {
+    console.log(document.getElementById("btn-4").innerText);
+    if (document.getElementById("btn-4").innerText === 'Learn More') {
+        document.getElementById("btn-4").innerText="Collapse";
+    } else {
+        document.getElementById("btn-4").innerText="Learn More";
+    }
+}
+
+function collapseSwitch5() {
+    console.log(document.getElementById("btn-5").innerText);
+    if (document.getElementById("btn-5").innerText === 'Learn More') {
+        document.getElementById("btn-5").innerText="Collapse";
+    } else {
+        document.getElementById("btn-5").innerText="Learn More";
+    }
+}
+
 function collapseSwitch6() {
     console.log(document.getElementById("btn-6").innerText);
-
     if (document.getElementById("btn-6").innerText === 'Learn More') {
         document.getElementById("btn-6").innerText="Collapse";
     } else {
